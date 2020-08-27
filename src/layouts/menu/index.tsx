8e02b76853230c7f534726e6fe9db5caf47bdb7b
hongwu.pu <hongwupu@gmail.com>
2020-08-27 11:07:03 +0800

    update

--- src/layouts/menu/index.tsx
+++ src/layouts/menu/index.tsx
@@ -1,53 +1,47 @@
-import React, { Component } from 'react'
-import classnames from 'classnames'
+import React, { Component } from 'react';
+import classnames from 'classnames';
 import { Link } from 'umi';
-import Icon from '@/components/icon'
+import Icon from '@/components/icon';
 
-import styles from './index.less'
+import styles from './index.less';
 
 interface PropsType {
-    width?: number;
-    location: any
+  width?: number;
+  location: any;
 }
 
 export default (props: PropsType) => {
+  const { width = 64, location } = props;
+  const { pathname } = location;
 
-    const { width = 64, location } = props
-    const { pathname } = location
+  const menu = [
+    // { title: '导航', icon: 'iconlink', url: '/tool/home' },
+    { title: '翻译', icon: 'iconfanyi', url: '/tool/en' },
+    // { title: '草稿', icon: 'icondraft', url: '/tool/draft' },
+    // { title: 'TODO', icon: 'icontodo', url: '/tool/todo' }
+  ];
 
-    console.log(location)
-
-    const menu = [{
-        title: '导航', icon: 'iconlink',
-        url: '/tool/home'
-    }, {
-        title: '翻译', icon: 'iconfanyi',
-        url: '/tool/en'
-    }, {
-        title: '草稿', icon: 'icondraft',
-        url: '/tool/draft'
-    }, {
-        title: 'TODO', icon: 'icontodo',
-        url: '/tool/todo'
-    }]
-
-    return (
-        <div className={styles.menuLeft} style={{width: width}}>
-            <ul className={styles.menuList}>
-                { menu.map(item => {
-                    const active = location.pathname.includes(item.url)
-                    return (
-                        <li className={classnames(styles.menuItem, active ? styles.menuActive : '')} 
-                            key={item.title}
-                            title={item.title}
-                        >
-                            <Link to={item.url}>
-                                <Icon className={styles.menuIcon} type={item.icon} /> 
-                            </Link>
-                        </li>
-                    )
-                }) }
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className={styles.menuLeft} style={{ width: width }}>
+      <ul className={styles.menuList}>
+        {menu.map(item => {
+          const active = location.pathname.includes(item.url);
+          return (
+            <li
+              className={classnames(
+                styles.menuItem,
+                active ? styles.menuActive : '',
+              )}
+              key={item.title}
+              title={item.title}
+            >
+              <Link to={item.url}>
+                <Icon className={styles.menuIcon} type={item.icon} />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
