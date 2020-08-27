0ff291d72cd1a72d16cc9d2c6e4df0b4d982f0c9
hongwu.pu <hongwupu@gmail.com>
2020-08-27 11:10:58 +0800

    默认翻译

--- config/router.ts
+++ config/router.ts
@@ -1,20 +1,40 @@
 export default [
-    {
-        path: '/',
+  {
+    path: '/',
+    exact: true,
+    redirect: '/tool/en',
+  },
+  {
+    path: '/tool',
+    component: '@/layouts/index',
+    routes: [
+      { path: '/tool', redirect: '/tool/en' },
+      {
+        path: '/tool/home',
         exact: true,
-        redirect: '/tool/home'
-    },
-    { 
-        path: '/tool',
-        component: '@/layouts/index',
-        routes: [
-            { path: '/tool', redirect: '/tool/home' },
-            { path: '/tool/home', exact: true, title: '首页', component: '@/pages/home' },
-            { path: '/tool/en', exact: true, title: '英汉翻译', component: '@/pages/en' },
-            { path: '/tool/draft', exact: true, title: '草稿纸', component: '@/pages/draft' },
-            { path: '/tool/todo', exact: true, title: '待办', component: '@/pages/todo' },
-            { component: '@/pages/404' }
-        ]
-    },
-    { component: '@/pages/404' }
-]
+        title: '首页',
+        component: '@/pages/home',
+      },
+      {
+        path: '/tool/en',
+        exact: true,
+        title: '英汉翻译',
+        component: '@/pages/en',
+      },
+      {
+        path: '/tool/draft',
+        exact: true,
+        title: '草稿纸',
+        component: '@/pages/draft',
+      },
+      {
+        path: '/tool/todo',
+        exact: true,
+        title: '待办',
+        component: '@/pages/todo',
+      },
+      { component: '@/pages/404' },
+    ],
+  },
+  { component: '@/pages/404' },
+];
